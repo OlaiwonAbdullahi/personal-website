608e4c4274ff60b404157bb58eedef01dedd4212
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-07-02 10:40:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-const SELECTORS = {
-	CURRENT_TIME: `[data-testid="currentTimeUTC"]`,
-	CURRENT_DAY: `[data-testid="currentDay"]`
-}
-const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const INTERVAL = 1000
+document.addEventListener('DOMContentLoaded', function() {
+    function updateTimeAndDay() {
+        const now = new Date();
+        const utcTime = now.toUTCString().split(' ')[4];
+        const dayOfWeek = now.toLocaleDateString('en-US', { weekday: 'long' })
 
-/** @type {number} */
-let timerId;
+        document.querySelector('[data-testid="currentTimeUTC"]').textContent = utcTime
+        document.querySelector('[data-testid="currentDay"]').textContent = dayOfWeek
+    }
 
-const updateInfo = () => {
-	//const currentDate = new Date()
-	const currentTime = currentDate.toUTCString();
-	const currentDay = currentDate.getDay();
-	document.querySelector(SELECTORS.CURRENT_TIME).textContent = currentTime;
-	document.querySelector(SELECTORS.CURRENT_DAY).textContent = DAYS[currentDay];
-}
-
-
-const run = () => {
-	timerId = setInterval(updateInfo, INTERVAL)
-}
-
-run()
+    updateTimeAndDay()
+    setInterval(updateTimeAndDay, 1000)
+})
